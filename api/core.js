47cce8f027e970b648372641167311130b00ed51
Gorurai54 <goru78.nex@gmail.com>
2026-09-12 07:30:25 +0530

    Create core.js

---
+++ api/core.js
@@ -0,0 +1,51 @@
+const ALLOWED_ORIGIN =
+    "https://appnetick-link.vercel.app";
+
+
+function cors(req, res) {
+
+    const origin =
+        req.headers.origin;
+
+
+    if (origin === ALLOWED_ORIGIN) {
+
+        res.setHeader(
+            "Access-Control-Allow-Origin",
+            ALLOWED_ORIGIN
+        );
+
+    }
+
+
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,POST,OPTIONS"
+    );
+
+
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+
+
+    res.setHeader(
+        "Vary",
+        "Origin"
+    );
+
+
+    if (req.method === "OPTIONS") {
+
+        res.status(204).end();
+
+        return true;
+    }
+
+
+    return false;
+}
+
+
+module.exports = cors;
